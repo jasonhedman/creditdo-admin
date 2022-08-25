004879813bea1a1084ddf3d118ea30004035676e
jason <jason@hedmans.org>
2022-08-25 17:45:52 -0500

    separate work toDos and dashboard toDos

--- src/components/work/ToDos/ToDoList.tsx
+++ src/components/work/ToDos/ToDoList.tsx
@@ -5,9 +5,10 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 
-import useToDos from '../../../hooks/useToDos'
 import { todoTabs } from '../../../hooks/types'
 import ToDo from '../../utility/ToDo';
+
+import useEventToDos from '../../../hooks/useEventToDos';
 
 interface Props {
     classId: string
@@ -15,9 +16,7 @@ interface Props {
 
 const ToDoList : FC<Props> = ({ classId }) => {
 
-    console.log(classId);
-
-    const { toDoBuckets, check, uncheck } = useToDos(classId);
+    const { toDoBuckets, check, uncheck } = useEventToDos(classId);
 
     const [tabValue, setTabValue] = useState<number>(0)
 
--- src/hooks/useEventToDos.ts
+++ src/hooks/useEventToDos.ts
@@ -4,13 +4,14 @@ import { collection, CollectionReference, doc, updateDoc, orderBy, query } from 
 
 import moment from "moment";
 
-import { ToDo, statuses } from "./types";
+import { ToDo, todoTabs } from "./types";
 
 type ToDoBuckets = {
-  [key in typeof statuses[number]]: ToDo[];
+  [key in typeof todoTabs[number]]: ToDo[];
 }
 
-const useToDos = (classId : string) => {
+
+const useEventToDos = (classId : string) => {
 
     const [toDos, loading, error] = useCollectionData<ToDo>(query(collection(db, "classes", classId, "toDos") as CollectionReference<ToDo>, orderBy('id', 'asc')));
 
@@ -24,20 +25,19 @@ const useToDos = (classId : string) => {
 
     const getToDoBuckets = () : ToDoBuckets => {
         const emptyBuckets : ToDoBuckets = {
-            'Not Started': [],
-            'Past Due': [],
+            'Before Event': [],
+            'After Event': [],
             'Completed': []
         }
         if(toDos) {
             return toDos.reduce((acc, toDo) => {
                 if (toDo.completed) {
                     acc['Completed'].push(toDo);
-                }
-                else {
-                    if(moment().isAfter(toDo.dueDate.seconds * 1000)){
-                        acc['Past Due'].push(toDo);
+                } else {
+                    if(toDo.beforeEvent){
+                        acc['Before Event'].push(toDo);
                     } else {
-                        acc['Not Started'].push(toDo);
+                        acc['After Event'].push(toDo);
                     }
                 }
                 return acc;
@@ -55,4 +55,4 @@ const useToDos = (classId : string) => {
     }
 }
 
-export default useToDos;
+export default useEventToDos;
